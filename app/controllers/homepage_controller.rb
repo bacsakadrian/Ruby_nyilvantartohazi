1128e15726748d7c4a20687e2ec3b11263725026
Adrián laptop <bacsakadrian@gmail.com>
2021-11-14 19:51:09 +0100

    paginate

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,13 +1,14 @@
 class HomepageController < ApplicationController
   def Index
-    @employees = Employee.all
+    @employees = Employee.paginate(:page => params[:page], per_page: 1)
   end
 
   def statistics
     @countall = Employee.all.count
-    @countboss = Employee.count(:isboss)
+    @countboss = Employee.where(:isboss => true).count
     @sum_days = Employee.sum(:days)
-
+    
+    @bossrate = 100*(@countboss.to_f / @countall.to_f)
     @avgdays = @sum_days / @countall
   end
 end
